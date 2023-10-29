baad4fc0b546e4bb4e3e25e158b04ee6d6b412ec
Sean Bethard <sean@seanbethard.net>
2023-10-29 05:10:35 -0500

    Merge biker-scum-dev.

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -11,7 +11,7 @@
           @click="toggleLeftDrawer"
         />
 
-        <q-toolbar-title> Portal </q-toolbar-title>
+        <q-toolbar-title></q-toolbar-title>
 
         <div>{{ $q.lang.nativeName }}</div>
       </q-toolbar>
@@ -19,7 +19,7 @@
 
     <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
       <q-list>
-        <q-item-label header> BIKER SCUM PORTAL </q-item-label>
+        <q-item-label header>Services</q-item-label>
 
         <EssentialLink
           v-for="link in essentialLinks"
@@ -41,15 +41,15 @@ import EssentialLink from 'components/EssentialLink.vue';
 
 const linksList = [
   {
-    title: 'Scumbag',
-    caption: 'Parts and recommendations for motorcycle mechanics.',
+    title: 'Repair Assistant',
+    caption: 'Procedures and recommendations',
     icon: 'mic',
-    link: 'https://www.bikerscum.ai/scumbag',
+    link: 'https://www.bikerscum.ai/repair',
   },
   {
-    title: 'Search',
-    caption: 'Metasearch engine for tools and aftermarket Harley parts.',
-    icon: 'saved_search',
+    title: 'Parts Search',
+    caption: 'Aftermarket metasearch',
+    icon: 'search',
     link: 'https://www.bikerscum.ai/search',
   },
   {
@@ -57,18 +57,6 @@ const linksList = [
     caption: 'Truett & Osborn',
     icon: 'brightness_5',
     link: 'http://www.truettandosborn.com/Flywheels.htm',
-  },
-  {
-    title: 'Engines',
-    caption: 'S&S Cycle',
-    icon: 'build',
-    link: 'https://www.sscycle.com/search/?category=1|Engines',
-  },
-  {
-    title: 'Discontinued Engines',
-    caption: 'Semigalls',
-    icon: 'build',
-    link: 'https://semigalls.com/sscycle-sh103-engine-complete-assembled-alt-gen-engine-for-1970-99-custom-chassis',
   },
 ];
 
